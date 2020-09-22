c8d32f3942eba952a64b9a24ec6568d94eb1c3df
luchovzla <luis.g.1990@gmail.com>
2020-09-22 15:14:41 -0500

    Modified script so it stored the resulting data set in a .csv file and formatted it appropriately.

--- Code/run_analysis.R
+++ Code/run_analysis.R
@@ -5,25 +5,34 @@ library(tidyverse)
 
 # Merge training and test sets to create one data set
 
-## Read needed files, assign descriptive variable names to columns 
+## Descriptive variable names 
 
 columnNames <- read_table("./UCI HAR Dataset/features.txt", col_names = FALSE) %>% 
     mutate(X1 = str_remove(X1, "[:digit:]{1,3} "))
+columnNamesVector <- columnNames$X1
 
-testValues <- read_table("./UCI HAR Dataset/test/X_test.txt", col_names = columnNames$X1)
-testValues$subject <- read_table("./UCI HAR Dataset/test/subject_test.txt", col_names = FALSE)
-testValues$activity <- read_table("./UCI HAR Dataset/test/y_test.txt", col_names = FALSE)
-trainValues <- read_table("./UCI HAR Dataset/train/X_train.txt", col_names = columnNames$X1)
-trainValues$subject <- read_table("./UCI HAR Dataset/train/subject_train.txt", col_names = FALSE)
-trainValues$activity <- read_table("./UCI HAR Dataset/train/y_train.txt", col_names = FALSE)
+## Create test data frame
 
-dataFrame <- bind_rows(testValues, trainValues) %>%
-    select(subject, activity, everything()) 
+testSubjects <- read_table("./UCI HAR Dataset/test/subject_test.txt", col_names = "subject")
+testActivities <- read_table("./UCI HAR Dataset/test/y_test.txt", col_names = "activity")
+testValues <- read_table("./UCI HAR Dataset/test/X_test.txt", col_names = columnNamesVector) %>%
+    bind_cols(testSubjects, testActivities, .)
+
+## Create train data frame
+
+trainSubjects <- read_table("./UCI HAR Dataset/train/subject_train.txt", col_names = "subject")
+trainActivities <- read_table("./UCI HAR Dataset/train/y_train.txt", col_names = "activity")
+trainValues <- read_table("./UCI HAR Dataset/train/X_train.txt", col_names = columnNamesVector) %>%
+    bind_cols(trainSubjects, trainActivities, .)
+
+## Merge datasets together
+
+dataFrame <- bind_rows(testValues, trainValues)
 
 # Extract mean and standard deviation for each measurement
 
 dataFrame <- dataFrame %>% 
-    select(1 | 2 | contains("std()") | contains("mean()"))
+    select(subject | activity | contains("std()") | contains("mean()"))
 
 # Use descriptive activity names to name the activities in the dataset
 
@@ -50,4 +59,8 @@ averagesDataFrame <- dataFrame %>%
     group_by(subject, activity) %>%
     summarize(across(1:66, mean))
 
+# Display summarized dataset and store in a csv file
+
 View(averagesDataFrame)
+
+write_csv(averagesDataFrame, "./Output/results.csv")
